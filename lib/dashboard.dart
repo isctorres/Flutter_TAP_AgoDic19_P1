4a9280ebf323048f39465482a7b1cbfa35f9926e
isctorres <isctorres@gmail.com>
2019-11-05 18:06:28 -0600

    actualizacion

--- lib/dashboard.dart
+++ lib/dashboard.dart
@@ -23,9 +23,54 @@ class MenuLateralState extends State<MenuLateral>{
     return Scaffold(
       appBar: AppBar(
         title: Text('Conversor :)'),
+        backgroundColor: Colors.orange,
       ),
       body: Center(
         child: Image.asset("images/Lincito.jpg"),
+      ),
+      drawer: Drawer(
+        child: ListView(
+          children: <Widget>[
+            UserAccountsDrawerHeader(
+              decoration: BoxDecoration(
+                color: Colors.deepOrange
+              ),
+              accountName: Text('Rubensin :)'),
+              accountEmail: Text('ruben.torres@itcelaya.edu.mx'),
+              currentAccountPicture: CircleAvatar(
+                backgroundImage: NetworkImage('http://i.pravatar.cc/300'),
+              ),
+            ),
+            ListTile(
+              title: Text('Masa'),
+              trailing: Icon(Icons.all_inclusive),
+              onTap: (){
+
+              },
+            ),
+            ListTile(
+              title: Text('Volumen'),
+              trailing: Icon(Icons.all_inclusive),
+              onTap: (){
+                
+              },
+            ),
+            ListTile(
+              title: Text('Temperatura'),
+              trailing: Icon(Icons.all_inclusive),
+              onTap: (){
+                
+              },
+            ),
+            ListTile(
+              title: Text('Longitud'),
+              trailing: Icon(Icons.all_inclusive),
+              onTap: (){
+                
+              },
+            )
+          ],
+        ),
       ),
     );
   }
